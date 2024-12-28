99b4bcc0f51cbf1934322a4d15ff21987bf7c796
s-kudekar <shubham.kudekar@smartreach.io>
2024-12-28 18:55:15 +0530

    mvn test does Not support test classes to be named with Spec so Using Test

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class StringCalculatorSpec {
+public class StringCalculatorTest {
 
     @Test
     public void testAddEmptyString() {
